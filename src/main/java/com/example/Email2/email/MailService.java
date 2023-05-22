e8ab7c1b8defb7aabc0c3d3052596660c4f3ac00
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-22 16:31:39 +0200

    fixed formatting

--- src/main/java/com/example/Email2/email/MailService.java
+++ src/main/java/com/example/Email2/email/MailService.java
@@ -8,11 +8,8 @@ import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -25,10 +22,10 @@ public class MailService {
 	public void sendTo(String receiver, String subject, String text) throws MessagingException, IOException {
 		Email emailSender = new Email("tommasotranchese.dev@gmail.com");
 		Email emailReceiver = new Email(receiver);
-		String formattedText = "<h1>Hello World!</h1>" +
-				"<h2>You have a new message: </h2>" +
-				"<img src='https://assets.gadgets360cdn.com/img/crypto/dogecoin-og-logo.png' alt='Alternative text' height='200'>" +
-				"<h3>" + text + "</h3>";
+		String formattedText = "<h1>Hello World!</h1>"
+				+ "<h2>You have a new message: </h2>"
+				+ "<img src='https://assets.gadgets360cdn.com/img/crypto/dogecoin-og-logo.png' alt='Alternative text' height='200'>"
+				+ "<h3>" + text + "</h3>";
 		Content content = new Content("text/html", text);
 		Mail mail = new Mail(emailSender, subject, emailReceiver, content);
 		SendGrid sendGrid = new SendGrid(environment.getProperty("spring.sendgrid.api-key"));
